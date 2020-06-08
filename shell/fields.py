db7a809dd565b624dcff629e67a72960563525c6
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2020-06-08 15:03:22 +0200

    Includes fields and visualizations modules Updates library notebook.

---
+++ shell/fields.py
@@ -0,0 +1,90 @@
+"""
+Contain functions that allow constructing simple fields on a grid
+"""
+import astropy.units as u
+import numpy as np
+from galmag.util import derive
+pi = np.pi*u.rad
+sqrt2 = np.sqrt(2)
+
+def uniform(grid, B):
+    """Uniform field"""
+    return [Bi*np.ones(grid.shape) for Bi in B]
+
+def simple_random(grid, Brms):
+    """
+    Generates a basic random field
+    
+    Parameters
+    ----------
+    grid : imagine.fields.grid.Grid
+        An IMAGINE grid object
+    Brms : float
+        RMS value of the computed field 
+        
+    Returns
+    -------
+    Bx, By, Bz
+    """
+    mu = 0; sigma = 1
+    # Defines a random vector potential 
+    A_rnd = {} # Dictionary of vector components
+    for i, c in enumerate(('x','y','z')):
+        A_rnd[c] = np.random.normal(mu, sigma, grid.resolution.prod())
+        A_rnd[c] = A_rnd[c].reshape(grid.resolution)
+
+    # Prepares the derivatives to compute the curl
+    dBi_dj ={}
+    for i, c in enumerate(['x','y','z']):
+        for j, d in enumerate(['x','y','z']):
+            dj = (grid.box[j][-1]-grid.box[j][0])/float(grid.resolution[j])
+            dBi_dj[c,d] = derive(A_rnd[c],dj, axis=j)
+
+    # Computes the curl of A_rnd
+    Brnd = {}        
+    Brnd['x'] = dBi_dj['z','y'] -  dBi_dj['y','z']
+    Brnd['y'] = dBi_dj['x','z'] -  dBi_dj['z','x']
+    Brnd['z'] = dBi_dj['y','x'] -  dBi_dj['x','y']
+    
+    # Finds normalization factor  to obtain correct Brms
+    norm = np.sqrt(np.mean([Brnd[k]**2 for k in Brnd]))
+    f = Brms/norm/np.sqrt(3)
+    
+    return Brnd['x']*f, Brnd['y']*f,  Brnd['z']*f
+
+
+def helical(grid, B, period=70*u.pc):
+    """
+    Computes a simple helical field
+    
+    Parameters
+    ----------
+    B : list
+        List containing the x, y and z magnitudes
+    period
+        Period of the helical field
+    
+    Returns
+    -------
+    Bx, By, Bz
+    """
+    
+    # Helical parallel to x
+    Bx = np.ones(grid.x.shape) /sqrt2  * B[0]
+    arg = pi*grid.x/period
+    By = np.cos(arg) /sqrt2 * B[0]
+    Bz = np.sin(arg) /sqrt2 * B[0]
+
+    # Helical parallel to y
+    arg = pi*grid.y/period
+    Bx += np.ones(grid.y.shape) /sqrt2  * B[1]
+    By += np.cos(arg) /sqrt2 * B[1]
+    Bz += np.sin(arg) /sqrt2 * B[1]
+
+    # Helical parallel to z
+    arg = pi*grid.z/period
+    Bx += np.ones(grid.z.shape) /sqrt2  * B[2]
+    By += np.cos(arg) /sqrt2 * B[2]
+    Bz += np.sin(arg) /sqrt2 * B[2]
+
+    return Bx, By, Bz
---
+++ shell/visualization.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def get_latex_units(q):
+    return q.unit._repr_latex_().replace('$','')
+
+def plot_scalar_xy(grid, scalar_field, name='n',
+                   pos=None, ax=None, fig=None, **kwargs):
+    """
+    Plots a slice of a scalar field defined on a 3D cartesian grid
+    """
+    if ax is None:
+        if fig is None:
+            fig = plt.figure(dpi=150)
+        ax = fig.add_subplot(111)
+        
+    if pos is None:
+        # If no position is supplied, take the middle!
+        pos = grid.resolution[2]//2
+        
+    if len(scalar_field.shape)==3:
+        im_slice = scalar_field[:,:,pos]
+    else:
+        im_slice = scalar_field
+    
+    im = ax.contourf(grid.x[:,:,pos], grid.y[:,:,pos], im_slice, 
+                     **kwargs)
+    ax.set_aspect(1)
+    ax.set_xlabel(r'$x\;[\rm pc]$')
+    ax.set_ylabel(r'$y\;[\rm pc]$')
+    cax = plt.colorbar(im, ax=ax)
+    cax.set_label(r'${}\;\left[\,{}\,\right]$'.format(name,
+                                  get_latex_units(scalar_field)))
+    return fig
+    
+def plot_vector_xy(grid, vector_field, skip=3, name=r'\mathbf{B}',
+                   pos=None, ax=None, fig=None, show_z_component=False,
+                   quiver_color='orange', contour_alpha=1,**kwargs):
+    """
+    Plots a slice of a vector field defined on a 3D cartesian grid
+    """
+    if ax is None:
+        if fig is None:
+            fig = plt.figure(dpi=150)
+        ax = fig.add_subplot(111)
+    
+    if pos is None:
+        # If no position is supplied, take the middle!
+        pos = grid.resolution[2]//2
+        
+    Bx, By, Bz = vector_field
+    if show_z_component:
+        B = Bz
+        vmax = np.max(np.abs(Bz.value))
+        if ('vmin' not in kwargs) and ('vmax' not in kwargs):
+            kwargs['vmin'] = -vmax
+            kwargs['vmax'] = vmax
+        if 'cmap' not in kwargs:
+            kwargs['cmap'] = 'coolwarm'
+    else:
+        B = np.sqrt(Bx**2+By**2+Bz**2)
+        name = '|'+name+'|'
+    
+    im = ax.contourf(grid.x[:,:,pos],grid.y[:,:,pos], B[:,:,pos], 
+                     alpha=contour_alpha, **kwargs)
+    # Quiver does not handle units well. Does, we select the values instead
+    ax.quiver(grid.x[::skip,::skip,pos].value, grid.y[::skip,::skip,pos].value, 
+           Bx[::skip,::skip,pos].value, By[::skip,::skip,pos].value, color=quiver_color)
+
+    ax.set_aspect(1)
+    ax.set_xlabel(r'$x\;[\rm pc]$')
+    ax.set_ylabel(r'$y\;[\rm pc]$')
+    cax = plt.colorbar(im, ax=ax)
+    cax.set_label(r'${}\;\left[\,{}\,\right]$'.format(name, get_latex_units(B)))
+    return fig
